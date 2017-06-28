39406936db87c1650376a1179e878686011141d4
achicha <meth787@gmail.com>
2017-06-28 13:31:39 +0300

    lesson3

--- firstapp/article/admin.py
+++ firstapp/article/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import Article, Comments
+
 
 # Register your models here.
+class ArticleInline(admin.StackedInline):
+    """
+    add comments field to Admin page
+    """
+    model = Comments
+    extra = 2
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    This is settings for Article
+    """
+    fields = ['article_title', 'article_text', 'article_date']  # only these fields will be shown in admin page
+    inlines = [ArticleInline]                                   # constrains that shows Articles
+    list_filter = ['article_date']                              # filter out Articles in Admin page by date
+
+admin.site.register(Article, ArticleAdmin)  # register Article for Admin app
--- firstapp/article/models.py
+++ firstapp/article/models.py
@@ -5,7 +5,17 @@ from django.db import models
 class Article(models.Model):
     class Meta:
         db_table = "article"
+
     article_title = models.CharField(max_length=200)
     article_text = models.TextField()
     article_date = models.DateTimeField()
-    article_likes = models.IntegerField()
+    article_likes = models.IntegerField(default=0)
+
+
+class Comments(models.Model):
+    class Meta:
+        db_table = "comments"
+
+    comments_text = models.TextField()
+    # one-to-many relationships: 1 article -> 10 comments
+    comments_article = models.ForeignKey(Article)
