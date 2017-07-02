23ce03543b7f6ebaf2e0baeb4232f215eb4fd473
achicha <meth787@gmail.com>
2017-07-02 14:20:38 +0300

    lesson9_1: cookie files

--- firstapp/article/views.py
+++ firstapp/article/views.py
@@ -38,8 +38,8 @@ def article(request, article_id=1):
     comment_form = CommentForm
     args = {}
     args.update(csrf(request))
-    args['article'] = Article.objects.get(id=article_id)
-    args['comments'] = Comments.objects.filter(comments_article_id=article_id)
+    args['article'] = Article.objects.get(id=article_id)                        # get only 1 result
+    args['comments'] = Comments.objects.filter(comments_article_id=article_id)  # filter possible many results
     args['form'] = comment_form
     return render_to_response('article.html', args)
 
@@ -47,20 +47,24 @@ def article(request, article_id=1):
 def addlike(request, article_id):
     """add likes to article"""
     try:
-        article = Article.objects.get(id=article_id)
-        article.article_likes += 1
-        article.save()
+        if article_id in request.COOKIES:
+            return redirect('/')
+        else:
+            article = Article.objects.get(id=article_id)
+            article.article_likes += 1
+            article.save()
+            response = redirect('/')
+            response.set_cookie(article_id, "test")  # save {1:'test'} in our cookie file
+            return response
     except ObjectDoesNotExist:
         raise Http404
-
-    return redirect('/')
 
 
 def addcomment(request, article_id):
     if request.POST:
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
+        form = CommentForm(request.POST)        # only post requests possible
+        if form.is_valid():                     # form validation
+            comment = form.save(commit=False)   # not save comment automatically
             comment.comments_article = Article.objects.get(id=article_id)
-            form.save()
+            form.save()                         # save in DB
     return article(request, article_id)
