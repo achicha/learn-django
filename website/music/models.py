c3a3312c1f4d0fd2997ec5852ae26c9bdda6f2f1
achicha <meth787@gmail.com>
2017-07-05 19:16:19 +0300

    2_11: register Album in admin.py

--- website/music/models.py
+++ website/music/models.py
@@ -7,6 +7,9 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
 
 class Song(models.Model):
     # on_delete = if Album will be deleted, delete all linked Songs too.
