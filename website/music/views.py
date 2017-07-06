c9ba724d9188b81d1d0e882d5776eee8af4ce773
achicha <meth787@gmail.com>
2017-07-06 13:09:04 +0300

    2_15: templates 2

--- website/music/views.py
+++ website/music/views.py
@@ -1,16 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('index.html')
     context = {
         'all_albums': all_albums,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def detail(request, album_id):
